4189119f24e5918d3c8ef5a8ca6e7362e082e947
Kavya Goudar <ammuk2310@gmail.com>
2022-04-19 14:17:37 +0530

    Web API-MVC

---
+++ BookDotnetCore.API/Controllers/BookDetailsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BookDotnetCore.BAL.services;
+using BookDotnetCore.Entity;
+
+namespace BookDotnetCore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookDetailsController : ControllerBase
+    {
+        private BookDetailsService _bookDetailsService;
+        public BookDetailsController(BookDetailsService bookDetailsService)
+        {
+            _bookDetailsService = bookDetailsService;
+        }
+        [HttpPost]
+
+        public IActionResult SaveBook(BookDetails bookDetails)
+        {
+            return Ok(_bookDetailsService.SaveBook(bookDetails));
+        }
+    }
+}
+
+//swagger:needed to run api services
+//postman:
