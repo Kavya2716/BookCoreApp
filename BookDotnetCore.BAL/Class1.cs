78469849ff710b9a7a6a5f52f847aabde96782c9
Kavya Goudar <ammuk2310@gmail.com>
2022-04-19 14:18:35 +0530

    BAL-Class Library

---
+++ BookDotnetCore.BAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookDotnetCore.BAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ BookDotnetCore.BAL/services/BookDetailsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookDotnetCore.DAL.Repositories;
+using BookDotnetCore.Entity;
+namespace BookDotnetCore.BAL.services
+{
+    public class BookDetailsService
+    {
+        private IBookRepository _bookRepository;
+        public BookDetailsService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        public string SaveBook(BookDetails bookDetails)
+        {
+            return _bookRepository.SaveBook(bookDetails); 
+        }
+    }
+}
