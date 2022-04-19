41f18285fa4675d2910fb02433b7c7a04f4efe89
Kavya Goudar <ammuk2310@gmail.com>
2022-04-19 14:20:03 +0530

    Entities

---
+++ BookDotnetCore.Entity/BookDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BookDotnetCore.Entity
+{
+    public class BookDetails
+    {
+        [Key]//pk
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//identity column
+        public int BookId { get; set; } //no need to enter any value since it is identity column
+        public string BookName { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int Price { get; set; }
+        public string BookCategory { get; set; }
+    }
+}
